4719ead35ab878c203fd3fe2ee0a5ec7f3040f00
Resovijay <94854436+Resovijay@users.noreply.github.com>
2024-07-26 02:09:42 +0530

    path

--- server/index.js
+++ server/index.js
@@ -41,14 +41,14 @@ dbConnect();
 const cloudinary = require("./configuration/cloudinary");
 cloudinary.cloudinaryConnect();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 // });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
 
